2b5cf3744fabb0c840f1fca9b4510b84c4dff493
AndreyLevin1780 <andreylevin1980@mail.ru>
2024-06-26 22:33:00 +0300

    Initial commit

---
+++ src/test/java/sky/pro/Kurs2/Kurs2ApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.Kurs2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kurs2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
